288e1166758b1a2bc02229102fd219c75926431f
daliang987 <daliang987@126.com>
2018-01-21 01:36:42 +0800

    modfiy pass

--- application/admin/controller/Entry.php
+++ application/admin/controller/Entry.php
@@ -3,10 +3,27 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use app\common\model\Admin;
 
 class Entry extends Common
 {
     public function index(){
+        return $this->fetch();
+    }
+
+    public function pass(){
+        
+        if(request()->isPost()){
+            // halt(input('post.'));
+            $res=(new Admin)->pass(input('post.'));
+            
+            if($res['valid']){
+                $this->success($res['msg'],'index');exit;
+            }else{
+                $this->error($res['msg']);exit;
+            }
+        }
+
         return $this->fetch();
     }
 }
--- application/common/model/Admin.php
+++ application/common/model/Admin.php
@@ -4,6 +4,7 @@ namespace app\common\model;
 
 use think\Model;
 use think\Loader;
+use think\Validate;
 
 class Admin extends Model
 {
@@ -27,5 +28,40 @@ class Admin extends Model
         session('admin.admin_username',$userinfo['admin_username']);
 
         return ['valid'=>1,'msg'=>'登录成功'];
+    }
+
+
+    public function pass($data){
+
+        $validate = new Validate([
+            'admin_password' => 'require',
+            'admin_newpass' => 'require',
+            'confirm_newpass' => 'require|confirm:admin_newpass',
+        ],[
+            'admin_password.require' => '请输入原始密码',
+            'admin_newpass.require' => '请输入新密码',
+            'confirm_newpass.require' => '确认新密码不能为空',
+            'confirm_newpass.confirm' => '两次密码输入不一致',
+        ]);
+
+        if (!$validate->check($data)) {
+            return ['valid'=>0,'msg'=>($validate->getError())];
+        }
+
+        $userinfo=$this->where('admin_id',session('admin.admin_id'))->where('admin_password',md5($data['admin_password']))->find();
+        if(!$userinfo){
+            return ['valid'=>0,'msg'=>'原始密码不正确'];
+        }
+        // halt($userinfo);
+        $res=$this->save(['admin_password'=>md5($data['admin_newpass'])],['admin_id'=>session('admin.admin_id')]);
+
+        if($res){
+            return ['valid'=>1,'msg'=>'密码修改成功'];
+        }else{
+            return ['valid'=>0,'msg'=>'密码修改失败'];
+        }
+
+        
+
     }
 }
